b9c3338c64ce4ac28520340eef833eb66045cda0
pidandan <809042368@qq.com>
2021-09-24 08:01:22 +0800

    路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,22 +1,57 @@
 import React from 'react'
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  RouteComponentProps,
+  Redirect,
+} from 'react-router-dom'
 import { router as routesMap } from './routers'
 import { IRouteTypes } from './routeTypes'
 const Routes: React.FC = () => {
   return (
     <Router>
       <Switch>
-        {routesMap.map((item: IRouteTypes, index: number) => {
-          return (
-            <Route
-              key={index}
-              render={(props) => {
-                const Component: any = item.component
-                return <Component {...props} route={item} />
-              }}
-            ></Route>
-          )
-        })}
+        {
+          // routesMap.map((item: IRouteTypes, index: number) => {
+          //   return (
+          //     <Route
+          //       key={index}
+          //       path={item.path}
+          //       exact={item.exact}
+          //       component={item.component}
+          //     ></Route>
+          //   )
+          // })
+
+          routesMap.map((item: IRouteTypes, index: number) => {
+            return (
+              <Route
+                key={index}
+                path={item.path}
+                exact={item.exact}
+                render={(props: RouteComponentProps) => {
+                  let Component: any = item.component
+                  console.log(item.children)
+                  //如果是不需要权限 或者 已登录 或者 访问路径是/login，则直接返回当前组件
+                  if (!item.requiresAuth) {
+                    return <Component {...props} route={item} />
+                  }
+                  debugger
+                  //否则重定向到/login
+                  return (
+                    <Redirect
+                      to={{
+                        pathname: '/login',
+                        state: { from: props.location },
+                      }}
+                    />
+                  )
+                }}
+              />
+            )
+          })
+        }
       </Switch>
     </Router>
   )
--- src/router/routers.ts
+++ src/router/routers.ts
@@ -1,10 +1,13 @@
 import { lazy } from 'react'
-import { AndroidOutlined } from '@ant-design/icons'
+// import { AndroidOutlined } from '@ant-design/icons'
 import { IRouteTypes } from './routeTypes'
 const Login = lazy(() => import('../page/login'))
 const Home = lazy(() => import('../page/home'))
 const User = lazy(() => import('../page/user'))
 const NotFound = lazy(() => import('../page/notFound'))
+// import Login from '../page/login'
+// import Home from '../page/home'
+// import NotFound from '../page/notFound'
 
 export const router: IRouteTypes[] = [
   {
@@ -17,19 +20,20 @@ export const router: IRouteTypes[] = [
     path: '/home',
     exact: true,
     component: Home,
-    requiresAuth: true,
+    requiresAuth: false,
     children: [
       {
         path: '/home/user',
         name: 'user',
-        icon: AndroidOutlined,
+        exact: true,
         component: User,
+        requiresAuth: false,
       },
     ],
   },
   {
     path: '*',
-    exact: true,
+    exact: false,
     component: NotFound,
     requiresAuth: false,
   },
